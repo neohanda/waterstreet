345542fb218c4da9a9be013f4e75d368bc2eadd7
Han Phan <none@none>
2015-10-17 01:09:54 -0500

    Start.

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php require_once('_header.php'); ?>
+
+<div id="containerannualmtg" class="group mtgIntro">
+	
+	<img src="http://waterstreet.com/wp-content/themes/waterstreet/img/waterstreet_logo.png" width="185" height="22" alt="Wst_logo" class="printOnly">
+
+	<div id="leftCol">
+		<img src="images/sidebar.jpg" alt="Annual Meeting" width="339" height="675" />
+	</div><!--end leftCol-->
+	
+	<div id="rightCol">
+		<h2>Please join us for the<br><span>Water Street Annual Meeting</span><br>Thursday, November 20<br>University Club of Chicago<br>76 East Monroe Street<br>
+			<div class="buttonWrapper">
+				<a href="register.php" class="submit" style="margin-right: 35px;">Register</a><a href="decline.php" class="submit">Unable to Attend</a>
+			</div>
+		</h2>
+		
+		<ul>
+			<li><span class="inlineTitle">Breakfast</span>&nbsp;<span class="inlineTime">8:00&thinsp;&ndash;&thinsp;8:30</span>&nbsp;&nbsp;in the Monroe Room</li>
+			<li><span class="inlineTitle">Meeting</span>&nbsp;<span class="inlineTime">8:30&thinsp;&ndash;&thinsp;12:30</span>&nbsp;&nbsp;in the Michigan Room</li>
+			<li><span class="inlineTitle">Lunch</span>&nbsp;<span class="inlineTime">12:30&thinsp;&ndash;&thinsp;1:30</span>&nbsp;&nbsp;in the Monroe Room</li>
+		</ul>
+		
+		<p>For your convenience, a block of hotel rooms has&nbsp;been reserved at The University Club. Upon&nbsp;registration, a confirmation email with hotel instructions will be sent to you.</p>
+		
+		<p class="notes">For more information contact Carrie Raffin, 312.506.2909, or email <a href="mailto:carrie.raffin@waterstreet.com">carrie.raffin@waterstreet.com</a>.</p>
+		
+	</div><!--end rightCol-->
+
+</div><!--end containerannualmtg-->
+
+<?php require_once('_footer.php'); ?>
